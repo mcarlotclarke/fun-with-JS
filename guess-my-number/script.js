d967c85c7c0f2243c2c5a3dc5a0f69d05a8dddfc
MCClarke <mcarlotclarke@gmail.com>
2021-11-01 22:10:54 -0400

    Implement game logic

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
+const checkNumber = () => {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct number! 🎉';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '⬆️ Too high';
+      score -= 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        'Sorry, you lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '⬇️ Too low';
+      score -= 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        'Sorry, you lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+};
+
+document.querySelector('.check').addEventListener('click', checkNumber);
