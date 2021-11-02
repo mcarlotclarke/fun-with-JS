410769eb1abb90503adc1e9b0d0886c12cf9f043
MCClarke <mcarlotclarke@gmail.com>
2021-11-02 09:02:50 -0400

    Reset game

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -1,17 +1,21 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
-document.querySelector('.number').textContent = secretNumber;
 
 const checkNumber = () => {
   const guess = Number(document.querySelector('.guess').value);
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No number!';
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct number! 🎉';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '⬆️ Too high';
@@ -22,6 +26,7 @@ const checkNumber = () => {
         'Sorry, you lost the game!';
       document.querySelector('.score').textContent = 0;
     }
+    // When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '⬇️ Too low';
@@ -35,4 +40,16 @@ const checkNumber = () => {
   }
 };
 
+const resetGame = () => {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+};
+
 document.querySelector('.check').addEventListener('click', checkNumber);
+document.querySelector('.again').addEventListener('click', resetGame);
