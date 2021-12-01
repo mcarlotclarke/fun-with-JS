365ec8a9bb307e4ac6f75a72180c10e456abc50e
MCClarke <mcarlotclarke@gmail.com>
2021-12-01 08:54:09 -0500

    Implement scrolling and page navigation

--- bankist-site/README.md
+++ bankist-site/README.md
@@ -1,3 +1,3 @@
 # Bankist
 
-A fictional marketing website for the fictional bank "Bankist", built using vanilla JS, HTML and CSS.
+A fictional marketing website for the minimalist bank "Bankist", built using vanilla JS, HTML and CSS.
--- bankist-site/script.js
+++ bankist-site/script.js
@@ -1,11 +1,14 @@
 'use strict';
 
-// Modal window
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const navLinks = document.querySelector('.nav__links');
 
+// Modal window
 const openModal = (e) => {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -17,7 +20,6 @@ const closeModal = () => {
   overlay.classList.add('hidden');
 };
 
-// Event listeners
 btnsOpenModal.forEach((btn) => btn.addEventListener('click', openModal));
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -27,3 +29,22 @@ document.addEventListener('keydown', (e) => {
     closeModal();
   }
 });
+
+// Button scroll
+const scrollTo = () => {
+  section1.scrollIntoView({ behavior: 'smooth' });
+};
+
+btnScrollTo.addEventListener('click', scrollTo);
+
+// Page navigation (using event delegation with the parent element)
+const smoothScroll = (e) => {
+  e.preventDefault();
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+};
+
+navLinks.addEventListener('click', smoothScroll);
