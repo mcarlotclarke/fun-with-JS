b92643f4105cee691dd15279554951cbb080dc78
MCClarke <mcarlotclarke@gmail.com>
2021-12-02 09:05:14 -0500

    Show operations tab messages

--- bankist-site/script.js
+++ bankist-site/script.js
@@ -7,6 +7,9 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 const navLinks = document.querySelector('.nav__links');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 // Modal window
 const openModal = (e) => {
@@ -48,3 +51,21 @@ const smoothScroll = (e) => {
 };
 
 navLinks.addEventListener('click', smoothScroll);
+
+// Tabbed component
+// As above, we are adding an event listener to the parent element using event delegation instead of adding it forEach element - which would slow down site performance if we had many elements.
+const showTab = (e) => {
+  const clicked = e.target.closest('.operations__tab');
+  // Guard clause: if you click anywhere else but the buttons
+  if (!clicked) return;
+  // Active tab
+  tabs.forEach((tab) => tab.classList.remove('operations__tab--active'));
+  tabsContent.forEach((c) => c.classList.remove('operations__content--active'));
+  clicked.classList.add('operations__tab--active');
+  // Tab content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+};
+
+tabsContainer.addEventListener('click', showTab);
